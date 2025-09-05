dddebc9ca842a71b124941fae45d7e8b10d10da1
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2025-09-04 21:47:15 -0700

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -224,11 +224,9 @@ $(document).ready(function () {
           return;
         }
         var copyButton = document.createElement("button");
-        // copyButton.title = "Copy to clipboard";
-        copyButton.title = "";
+        copyButton.title = "Copy to clipboard";
         copyButton.className = "clipboard-copy-button";
-        // copyButton.innerHTML = '<span class="sr-only">Copy code</span><i class="far fa-fw fa-copy"></i><i class="fas fa-fw fa-check copied"></i>';
-        copyButton.innerHTML = '<span class="sr-only"></span><i class="far fa-fw fa-copy"></i><i class="fas fa-fw fa-check copied"></i>';
+        copyButton.innerHTML = '<span class="sr-only">Copy code</span><i class="far fa-fw fa-copy"></i><i class="fas fa-fw fa-check copied"></i>';
         copyButton.addEventListener("click", copyButtonEventListener);
         container.prepend(copyButton);
       });
